3ee10acfc8a824146f50a15364c415c24f26a55a
ali konuk <alikonuk1@gmail.com>
2022-02-13 18:50:10 +0300

    before deploy

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = 0xeD182959386E0036AC79908df0F3E1d0791C8a5E;
+const WHITELIST_CONTRACT_ADDRESS = "0xeD182959386E0036AC79908df0F3E1d0791C8a5E";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
--- hardhat-tutorial/contracts/CryptoDevs.sol
+++ hardhat-tutorial/contracts/CryptoDevs.sol
@@ -3,7 +3,7 @@
 
   import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
   import "@openzeppelin/contracts/access/Ownable.sol";
-  import "IWhitelist.sol";
+  import "./IWhitelist.sol";
 
   contract CryptoDevs is ERC721Enumerable, Ownable {
       /**
